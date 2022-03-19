83727726604b1d53ebead042df0fa2ebdf9400f0
ArturPromon <artur.shovkunenko@gmail.com>
2022-03-19 20:30:56 +0200

    test

---
+++ 16032022/index.js
@@ -0,0 +1,4 @@
+function myFunction(){
+  document.getElementById("demo").innerHTML = "Artur";
+  console.log("Hello world!");
+}
---
+++ 16032022/index3.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.color = "red";
+}
+function myFunction2() {
+  let x = document.getElementById("demo");
+  x.style.color = "blue";
+}
+function myFunction3() {
+  let x = document.getElementById("demo");
+  x.style.color = "green";
+}
