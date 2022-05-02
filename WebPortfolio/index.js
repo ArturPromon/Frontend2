41eb838e5df1f20c10fd44dec6ff625d054e5d4e
ArturPromon <artur.shovkunenko@gmail.com>
2022-05-02 19:58:08 +0300

    changes

--- WebPortfolio/index.js
+++ WebPortfolio/index.js
@@ -1,19 +1,18 @@
 window.onload = function(){
-<<<<<<< HEAD
+
   const img = document.querySelectorAll('.images');
   for(let i = 0; i<img.length;i++){
     img[i].addEventListener("click", addclass);
-=======
+
   const images = document.querySelectorAll('.images');
 
   for(let i = 0; i < images.length; i++){
     images[i].addEventListener("click", myfunc);
->>>>>>> 35abb610a72d01ac8047ec30850b9a58e9cd7553
   }
 
 }
 
-<<<<<<< HEAD
+
 function addclass(){
   const img = document.querySelectorAll('.images');
 
@@ -27,8 +26,7 @@ function addclass(){
         this.classList.toggle("imgsize");
     }
 
-=======
+
 function myfunc() {
     this.classList.toggle("imgsize");
->>>>>>> 35abb610a72d01ac8047ec30850b9a58e9cd7553
 }
